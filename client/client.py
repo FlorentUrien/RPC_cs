fce0cf71e79e37413304b908586fdaa46c1c15f0
florent.urien <florent.urien@scalian.com>
2023-03-13 15:53:20 +0100

    Avec un fastapi

--- client/client.py
+++ client/client.py
@@ -1,6 +1,12 @@
 import xmlrpc.client
 
+
+
+
 print("bonjour 8020")
 proxy = xmlrpc.client.ServerProxy("http://server:8020/")
 result = proxy.add(7, 3)
 print(f"7 + 3 = {result}")
+
+
+
--- server/server.py
+++ server/server.py
@@ -1,4 +1,7 @@
 import xmlrpc.server
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 def add(x, y):
@@ -9,3 +12,8 @@ server = xmlrpc.server.SimpleXMLRPCServer(("0.0.0.0", 8020))
 print("Listening (0.0.0.0) on port 8020...")
 server.register_function(add, "add")
 server.serve_forever()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
