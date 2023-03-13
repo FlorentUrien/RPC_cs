606fd6e8b8bab294f3d61da7243ceb378b065fb4
florent.urien <florent.urien@scalian.com>
2023-03-13 16:47:50 +0100

    multithreaddé...

--- client/client.py
+++ client/client.py
@@ -1,12 +1,9 @@
 import xmlrpc.client
 
-
-
-
+print("toto")
 print("bonjour 8020")
-proxy = xmlrpc.client.ServerProxy("http://server:8020/")
+proxy = xmlrpc.client.ServerProxy("http://serveur:8020/")
 result = proxy.add(7, 3)
 print(f"7 + 3 = {result}")
-
-
-
+result = proxy.add(9, 4)
+print(f"9 + 4 = {result}")
--- server/server.py
+++ server/server.py
@@ -1,19 +1,30 @@
+import multiprocessing
 import xmlrpc.server
 from fastapi import FastAPI
-
-app = FastAPI()
-
-
-def add(x, y):
-    return x + y
+import uvicorn
 
 
-server = xmlrpc.server.SimpleXMLRPCServer(("0.0.0.0", 8020))
-print("Listening (0.0.0.0) on port 8020...")
-server.register_function(add, "add")
-server.serve_forever()
+def xmlrpc_server():
+    def add(x, y):
+        return x + y
+
+    server = xmlrpc.server.SimpleXMLRPCServer(("0.0.0.0", 8020))
+    server.register_function(add, "add")
+    server.serve_forever()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def fastapi_server():
+    app = FastAPI()
+
+    @app.get("/")
+    def read_root():
+        return {"Hello": "Worldmp"}
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    p1 = multiprocessing.Process(target=xmlrpc_server)
+    p2 = multiprocessing.Process(target=fastapi_server)
+    p1.start()
+    p2.start()
